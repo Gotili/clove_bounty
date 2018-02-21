ca63398e42ff21a60a8ce6bc4e3c5949731b8fb3
Felix B <mail@bourier.biz>
2018-02-21 17:23:36 +0100

    Added 25 Altcoins

--- network/shadowcash.py
+++ network/shadowcash.py
@@ -3,13 +3,20 @@ from clove.network.bitcoin import Bitcoin
 
 class ShadowCash(Bitcoin):
     """
-    Class with all the necessary ShadowCash (SDC) network information based on
+    Class with all the necessary ShadowCash network information based on
     https://github.com/shadowproject/shadow/blob/master/src/chainparams.cpp
-    (date of access: 02/17/2018)
+    (date of access: 02/21/2018)
     """
     name = 'shadowcash'
     symbols = ('SDC', )
-    seeds = ('seed.shadow.cash', 'seed2.shadow.cash', 'seed3.shadow.cash', 'seed4.shadow.cash', 'seed.shadowproject.io', 'seed.shadowchain.info')
+    seeds = ("seed.shadow.cash",
+             "seed2.shadow.cash",
+             "seed3.shadow.cash",
+             "seed4.shadow.cash",
+             "seed.shadowproject.io",
+             "seed.shadowchain.info")
     port = 51737
-
+	
 # no testnet
+	
+	
