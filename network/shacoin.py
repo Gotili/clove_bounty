7fc233d55ff0db8fc16565c5e95a7c609ac47838
Felix B <mail@bourier.biz>
2018-02-21 17:25:05 +0100

    Added 24 Altcoins

--- network/shacoin.py
+++ network/shacoin.py
@@ -1,25 +1,16 @@
-
 from clove.network.bitcoin import Bitcoin
 
 
 class SHACoin(Bitcoin):
     """
-    Class with all the necessary SHA network information based on
-    https://github.com/antibitcoin/antibitcoin-source/blob/master/src/net.cpp
-    (date of access: 02/12/2018)
+    Class with all the necessary SHACoin network information based on
+    https://github.com/AltcoinsBeta/SHACoin-Dead/blob/master/src/net.cpp
+    (date of access: 02/21/2018)
     """
     name = 'shacoin'
     symbols = ('SHA', )
-    seeds = ('seed1.shacoin.us', 'seed2.shacoin.us', 'seed3.shacoin.us')
+    seeds = ("seed1.shacoin.us",
+             "seed2.shacoin.us",
+             "seed3.shacoin.us")
     port = 25555
-
-
-class SHACoinTestNet(SHACoin):
-    """
-    Class with all the necessary SHA testing network information based on
-    https://github.com/antibitcoin/antibitcoin-source/blob/master/src/net.cpp
-    (date of access: 02/12/2018)
-    """
-    name = 'test-shacoin'
-    seeds = ()
-    port = 26555
+	
